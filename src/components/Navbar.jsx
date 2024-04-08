cbe6af4208e50b7542d0e2d8da94edb8866dc128
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2024-04-08 23:59:24 +0400

    add ROUTING with some style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,51 @@ import about from "../assets/about.png";
 import resume from "../assets/resume1.png";
 import works from "../assets/codesandbox.png";
 import contact from "../assets/contactImg.png";
+
+  
+import { NavLink } from "react-router-dom";
+const navLinkStyle = ({ isActive }) => ({
+  borderRadius: isActive ? "12px" : "16px",
+  background: isActive
+    ? "linear-gradient(153deg, #fa5252 16.67%, #dd2476 100%)"
+    : "transparent",
+});
+
 const Navbar = () => {
   return (
     <div className="navbar-menu">
-      <section className="current-about">
-        <img src={about} alt="" />
-        <span>About</span>
-      </section>
-      <section>
-        <img src={resume} alt="" />
-        <span>Resume</span>
-      </section>
-      <section>
-        <img src={works} alt="" />
-        <span>Projects</span>
-      </section>
-      <section>
-        <img src={contact} alt="" />
-        <span>Contact</span>
-      </section>
+      <NavLink
+        to="/"
+        className="current-about"
+        activeClassName="active-link"
+        style={navLinkStyle}
+      >
+        <section className="current-about">
+          <img src={about} alt="" />
+          <span>About</span>
+        </section>
+      </NavLink>
+
+      <NavLink to="/resume" activeClassName="active-link" style={navLinkStyle}>
+        <section>
+          <img src={resume} alt="" />
+          <span>Resume</span>
+        </section>
+      </NavLink>
+
+      <NavLink to="/works" activeClassName="active-link" style={navLinkStyle}>
+        <section>
+          <img src={works} alt="" />
+          <span>Projects</span>
+        </section>
+      </NavLink>
+
+      <NavLink to="/contact" activeClassName="active-link" style={navLinkStyle}>
+        <section>
+          <img src={contact} alt="" />
+          <span>Contact</span>
+        </section>
+      </NavLink>
     </div>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,6 +31,7 @@ const Contact = () => {
           </div>
         </form>
       </section>
+      <footer>© 2024 Giorgi Paksashvili ( Own Portfolio ).</footer>
     </div>
   );
 };
